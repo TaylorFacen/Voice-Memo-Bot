517092ab96a21be2010929dd3ba3b2229c5825fe
TaylorFacen <44216029+TaylorFacen@users.noreply.github.com>
2020-12-09 14:30:31 -0500

    Code for starting the conversation

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
+import json
+
 from flask import Flask, request
+from twilio.rest import Client
+
+from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_SYNC_SID
 
 app = Flask(__name__)
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 @app.route('/start_memo', methods = ['POST'])
 def start_memo():
     # Get data from request
+    data = request.form.to_dict()
+    api_url = request.host_url
 
     # Collect memo data from autopilot memory
+    memory = json.loads(data['Memory'])
+    twilio_number = memory['twilio']['sms']['To']
+    user_number = memory['twilio']['sms']['From']
+    responses = memory['twilio']['collected_data']['get_memo_data']['answers']
+    memo_title = responses['memo_title']['answer']
+    memo_tag = responses['memo_tag']['answer']
 
     # Call user
+    twiml = '''
+    <Response>
+        <Say>When you're done recording, press the pound key.</Say>
+        <Record timeout="10" transcribe="true" transcribeCallback="{}process_memo"/>
+        <Say>You'll receive a text when your memo is processed. Goodbye</Say>
+    </Response>
+    '''.format(api_url)
+
+    call = client.calls.create(
+        twiml = twiml,
+        to = user_number,
+        from_ = twilio_number
+    )
+
+    print(call.sid)
 
     # Create a sync list for the phone numebr if it doesn't already have one
 
--- config.py
+++ config.py
@@ -2,3 +2,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_SYNC_SID = os.environ['TWILIO_SYNC_SID']
